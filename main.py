1a0c9aa9fb576620f8e7274a4d68d21c2735dbb9
Pibloso <duncan.mccorkell@protonmail.com>
2025-03-10 22:14:00 -0600

    Add constants

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+from constants import *
+
+def main():
+    print(f"""
+Starting Asteroids!
+Screen wdith: {SCREEN_WIDTH}
+Screen height: {SCREEN_HEIGHT}
+    """)
+
+if __name__ == "__main__":
+    main()
